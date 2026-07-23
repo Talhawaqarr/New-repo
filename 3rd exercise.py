47e3a0e3c320fef283dfe39b58ad08ef8f00b614
Talhawaqarr <beastplayz79@gmail.com>
2026-07-23 22:00:26 +0500

    3rd exercise

---
+++ 3rd exercise.py
@@ -0,0 +1,4 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+print(f"The {adjective} {noun} decided to {verb} but failed because it was too {adjective}.")
