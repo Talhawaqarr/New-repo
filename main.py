984e5fb23ac3ca1f18582eeabe2c9cd1f83315f5
Talhawaqarr <beastplayz79@gmail.com>
2026-07-23 21:34:31 +0500

    first exercise

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world!")
+name = "Talha Waqar"
+age = 18
+height = 5.9
+drinks_tea = True
+print(f"Name: {name}", type(name))
+#print(type(name))
+print(f"Age: {age}", type(age) )
+#print(type(age))
+print(f"Height: {height}", type(height))
+#print(type(height))
+print(f"Drinks Tea: {drinks_tea}", type(drinks_tea))
+#print(type(drinks_tea))
